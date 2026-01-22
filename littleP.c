7ad3a0f25ee542848faf2c1d98d167d17ecdd38d
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-01-22 21:29:31 +0100

    Add password input and validation in littleP
    Implement password check for access control

---
+++ littleP.c
@@ -0,0 +1,19 @@
+#include "Calculator_Brain/main.h"
+#include <stdio.h>
+#include <string.h>
+/*This is for the package learning*/
+
+int littleP() {
+  char password [45];
+  printf("Enter Password: ");
+  scanf("%44s", password);
+  if(strcmp(password 'Lawrex+17') == 0) {
+    printf("Access Granted........\n");
+      printf("Cinari Labs Display **************/n");
+    return 1;
+  }
+  else {
+    printf("Access denied..........\n");
+  }
+  return 0;
+}
