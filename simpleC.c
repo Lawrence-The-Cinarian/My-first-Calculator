28caac28059302dbff6949620c5eee3665632458
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-02-07 07:57:58 +0100

    Update simpleC.c

--- simpleC.c
+++ simpleC.c
@@ -9,48 +9,50 @@ int simpleC() {
   double snum; //for representation of second number
   int choice;//for choices during switch/case creation
   do{
-  printf("******************************************************\n");
-  printf("Welcome To Cinari Lab's Calculator\n");
-  printf("******************************************************\n");
+  printf("╔════════════════════════════════════════════════════╗\n");
+  printf("║         Welcome To Cinari Lab's Calculator         ║\n");
+  printf("╚════════════════════════════════════════════════════╝\n");
   printf("\n");
-  printf("******************************************************\n");
-  printf("Enter 1 for using a NO. for square/roots, cubes/square\n");
-  printf("Enter 2 for using two NOs for operations(+, -, \\, *)\n");
-  printf("Enter number: ");
+  printf("╔════════════════════════════════════════════════════╗\n");
+  printf("║Use 1 for using a NO. for square/roots, cubes/square║\n");
+  printf("║Use 2 for using two NOs for operations(+, -, \\, *) ║\n");
+  printf("║                 Enter number: ");
   scanf("%d", &operand);
-  printf("******************************************************\n");
+  printf("╚════════════════════════════════════════════════════╝\n");
   printf("\n");
   if(operand <= 0 || operand >= 3) {
-   printf("Invalid Input\n");
+   printf("╔════════════════════════════════════════════════════╗\n");
+   printf("║                   Invalid Input                    ║\n");
+   printf("╚════════════════════════════════════════════════════╝\n");
    }
     
   else if(operand == 1) {
-   printf("******************************************************\n");
-   printf("Step 1: Enter NO. For Calculation\n");
-   printf("Step 2: Enter NO. in the options for calculations\n");
-   printf("(1) Square Root\n");
-   printf("(2) Cubic Root\n");
-   printf("(3) Square\n");
-   printf("(4) Cubic\n");
-   printf("******************************************************\n");
+   printf("╔════════════════════════════════════════════════════╗\n");
+   printf("║Step 1: Enter NO. For Calculation                   ║\n");
+   printf("║Step 2: Enter NO. in the options for calculations   ║\n");
+   printf("║                   (1) Square Root                  ║\n");
+   printf("║                   (2) Cubic Root                   ║\n");
+   printf("║                   (3) Square                       ║\n");
+   printf("║                   (4) Cubic                        ║\n");
+   printf("╚════════════════════════════════════════════════════╝\n");
    printf("\n");
-   printf("******************************************************\n");
-   printf("Enter Number for Calculation: ");
+   printf("╔════════════════════════════════════════════════════╗\n");
+   printf("║Enter Number for Calculation: ");
    scanf("%lf", &fnum);
-   printf("******************************************************\n");
+   printf("╚════════════════════════════════════════════════════╝\n");
    printf("\n");
-   printf("******************************************************\n");
-   printf("Enter Number from the options above: ");
+   printf("╔════════════════════════════════════════════════════╗\n");
+   printf("║Enter Number from the options above: ");
    scanf("%d", &choice);
-   printf("******************************************************\n");
+   printf("╚════════════════════════════════════════════════════╝\n");
    printf("\n");
     if(choice >= 1 && choice <= 4) {
       switch(choice) {
         case 1:
           if(fnum < 0) {
-        printf("******************************************************\n");
-        printf("Invalid Calculation\n");
-        printf("******************************************************\n");
+        printf("╔════════════════════════════════════════════════════╗\n");
+        printf("║                Invalid Calculation                 ║\n");
+        printf("╚════════════════════════════════════════════════════╝\n");
         printf("\n");
         return choice;
           }
@@ -59,9 +61,9 @@ int simpleC() {
 
         case 2:
           if(fnum < 0) {
-        printf("******************************************************\n");
-        printf("Invalid Calculation\n");
-        printf("******************************************************\n");
+        printf("╔════════════════════════════════════════════════════╗\n");
+        printf("║                Invalid Calculation                 ║\n");
+        printf("╚════════════════════════════════════════════════════╝\n");
         printf("\n");
         return choice;
           }
@@ -77,48 +79,48 @@ int simpleC() {
         break;
 
         default: 
-        printf("******************************************************\n");
-        printf("Invalid Input\n");
-        printf("******************************************************\n");
+        printf("╔════════════════════════════════════════════════════╗\n");
+        printf("║                   Invalid Input                    ║\n");
+        printf("╚════════════════════════════════════════════════════╝\n");
         printf("\n");
       }
-      printf("******************************************************\n");
-      printf("Ans: %.2f\n", answer);
-      printf("******************************************************\n");
+      printf("╔════════════════════════════════════════════════════╗\n");
+      printf("║Ans: %.2f\n", answer);
+      printf("╚════════════════════════════════════════════════════╝\n");
       printf("\n");
     }
     else {
-      printf("******************************************************\n");
-      printf("Program Terminated\n");
-      printf("******************************************************\n");
+      printf("╔════════════════════════════════════════════════════╗\n");
+      printf("║                 PROGRAM TERMINATED                 ║\n");
+      printf("╚════════════════════════════════════════════════════╝\n");
       printf("\n");
       return choice;
       }
   }
   else if(operand == 2) {
-    printf("******************************************************\n");
-    printf("Step 1: Enter Two NOs for Calculations\n");
-    printf("Step 2: Enter NO. in the options for calculations\n");
-    printf("(1) Addition\n");
-    printf("(2) Subtraction\n");
-    printf("(3) Multiplication\n");
-    printf("(4) Division\n");
-    printf("******************************************************\n");
+    printf("╔════════════════════════════════════════════════════╗\n");
+    printf("║Step 1: Enter Two NOs for Calculations              ║\n");
+    printf("║Step 2: Enter NO. in the options for calculations   ║\n");
+    printf("║                   (1) Addition                     ║\n");
+    printf("║                   (2) Subtraction                  ║\n");
+    printf("║                   (3) Multiplication               ║\n");
+    printf("║                   (4) Division                     ║\n");
+    printf("╚════════════════════════════════════════════════════╝\n");
     printf("\n");
-    printf("******************************************************\n");
+    printf("╔════════════════════════════════════════════════════╗\n");
     printf("Enter First Number: ");
       scanf("%lf", &fnum);
-    printf("******************************************************\n");
+    printf("╚════════════════════════════════════════════════════╝\n");
     printf("\n");
-    printf("******************************************************\n");
+    printf("╔════════════════════════════════════════════════════╗\n");
     printf("Enter Second Number: ");
       scanf("%lf", &snum);
-    printf("******************************************************\n");
+    printf("╚════════════════════════════════════════════════════╝\n");
     printf("\n");
-    printf("******************************************************\n");
+    printf("╔════════════════════════════════════════════════════╗\n");
     printf("Enter Number for Operation: ");
       scanf("%d", &choice);
-    printf("******************************************************\n");
+    printf("╚════════════════════════════════════════════════════╝\n");
     printf("\n");
     if(choice >=1 && choice <= 4) {
       switch(choice) {
@@ -136,9 +138,9 @@ int simpleC() {
 
         case 4:
         if(snum == 0) {
-          printf("******************************************************\n");
-          printf("Mathematical Error");
-          printf("******************************************************\n");
+      printf("╔════════════════════════════════════════════════════╗\n");
+      printf("║                 Mathematical Error                 ║\n");
+      printf("╚════════════════════════════════════════════════════╝\n");
           printf("\n");
           return choice;
         }
@@ -146,21 +148,21 @@ int simpleC() {
         break;
 
         default:
-          printf("******************************************************\n");
-          printf("Invalid Input\n");
-          printf("******************************************************\n");
+          printf("╔════════════════════════════════════════════════════╗\n");
+          printf("║                   Invalid Input                    ║\n");
+          printf("╚════════════════════════════════════════════════════╝\n");
           printf("\n");
           return choice;
       }
-        printf("******************************************************\n");
-        printf("Ans: %.2f\n", answer);
-        printf("******************************************************\n");
+        printf("╔════════════════════════════════════════════════════╗\n");
+        printf("║Ans: %.2f\n", answer);
+        printf("╚════════════════════════════════════════════════════╝\n");
         printf("\n");
     }
     else {
-      printf("******************************************************\n");
-      printf("Program Terminated");
-      printf("******************************************************\n");
+      printf("╔════════════════════════════════════════════════════╗\n");
+      printf("║                 PROGRAM TERMINATED                 ║\n");
+      printf("╚════════════════════════════════════════════════════╝\n");
       printf("\n");
       return choice;
       }
