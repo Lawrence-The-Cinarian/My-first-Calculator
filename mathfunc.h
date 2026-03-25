d035b9d4ed3bef1ba747fd01d7a0666206e42491
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-03-25 01:06:25 +0100

    Add mathfunc.h with basic arithmetic function declarations

---
+++ mathfunc.h
@@ -0,0 +1,15 @@
+#ifndef MATHFUNC_H
+#define MATHFUNC_H
+
+/*This is the full meanings
+add = addition
+sub = subtraction 
+mul = multiplication
+div = division*/
+
+double add(double a, double b);
+double sub(double a, double b);
+double mul(double a, double b);
+double div(double a, double b);
+
+#endif
