61a7d804c15e16404ecf053a04556c6b3c4f5302
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-03-25 01:08:42 +0100

    Implement basic arithmetic functions in mathfunc.c

--- mathfunc.c
+++ mathfunc.c
@@ -1,1 +1,14 @@
-#include
+#include "mathfunc.h"
+
+double add(double a, double b) {
+return a+b;
+}
+double sub(double a, double b) {
+return a-b;
+}
+double mul(double a, double b) {
+return a*b;
+}
+double div(double a, double b) {
+return a/b;
+}
